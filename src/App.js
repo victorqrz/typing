8f2e44d0ea41c49943fa03b33bb7cd68e086386f
Victor Queiroz <victoorqrz@gmail.com>
2021-09-15 16:01:11 -0300

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched">vic</span>
+        <span className="remainder">tor</span>
+      </div>
+
+      <div className="typed-keys">
+        asfdvicjeks
+      </div>
+
+      <div className="completed-words">
+        <ol>
+          <li>cidade</li>
+          <li>teste</li>
+          <li>destreza</li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default App;
