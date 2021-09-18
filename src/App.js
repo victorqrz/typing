34bd3379c94b952af91154efa51e0d1aa1f611f4
Victor Queiroz <victoorqrz@gmail.com>
2021-09-18 17:59:41 -0300

    storing typed keys

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const MAX_TYPED_KEYS = 30;
 
 const App = () => {
+  const [typedKeys, setTypedKeys] = useState([]);
+
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    const { key } = e;
+
+    setTypedKeys((prev) => [...prev, key].slice(MAX_TYPED_KEYS * -1));
+
+    console.log('key: ', key);
+  }
+
   return (
-    <div className="container">
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
         <span className="matched">vic</span>
         <span className="remainder">tor</span>
       </div>
 
       <div className="typed-keys">
-        asfdvicjeks
+        {typedKeys ? typedKeys.join(' ') : null}
       </div>
 
       <div className="completed-words">
